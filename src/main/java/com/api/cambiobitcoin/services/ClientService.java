b3f9c9ff07c17d3bc7bbf91b14818fc8a501e252
Filipe-uefs <lipe_0109@hotmail.com>
2022-05-05 13:25:28 -0300

    change method to add new client

--- src/main/java/com/api/cambiobitcoin/services/ClientService.java
+++ src/main/java/com/api/cambiobitcoin/services/ClientService.java
@@ -1,17 +1,20 @@
 package com.api.cambiobitcoin.services;
 
+import com.api.cambiobitcoin.models.AccountModel;
 import com.api.cambiobitcoin.models.ClientModel;
 import com.api.cambiobitcoin.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.math.BigDecimal;
 
 @Service
 public class ClientService {
 
     @Autowired
     ClientRepository clientRepository;
+    @Autowired AccountService accountService;
 
     public ClientModel getClientByCPF(String cpf) {
         return clientRepository.findByCpf(cpf);
@@ -23,7 +26,12 @@ public class ClientService {
 
     @Transactional
     public ClientModel createClient(ClientModel client) {
-        return clientRepository.save(client);
+        ClientModel newClient  = clientRepository.save(client);
+        AccountModel newAccount = new AccountModel();
+        newAccount.setClient(newClient);
+        newAccount.setBalance((double) 0);
+        newAccount.setQtdBitcoin(BigDecimal.valueOf(0));
+        return newClient;
     }
 
     @Transactional
